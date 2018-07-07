728f69b09a45f80a0f0e86a99188fd36ac679739
Tomasz Kaluzny <tkaluzny@outlook.com>
2018-07-07 21:46:58 +0100

    small refactor moved ReflectionExtensions class to its own file

--- Tangy/Extensions/IEnumerableExtensions.cs
+++ Tangy/Extensions/IEnumerableExtensions.cs
@@ -19,12 +19,4 @@ namespace Tangy.Extensions
                 };
         }
     }
-
-    public static class ReflectionExtensions
-    {
-        public static string GetPropertyValue<T>(this T item, string propertyName)
-        {
-            return item.GetType().GetProperty(propertyName).GetValue(item, null).ToString();
-        }
-    }
 }
