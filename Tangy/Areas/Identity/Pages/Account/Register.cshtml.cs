c52a21102b6a2f4eb018c86e1dabb646ec58206a
Tomasz Kaluzny <tkaluzny@outlook.com>
2018-07-12 01:50:23 +0200

    fixed the styling issue most probably introduced by identity scaffolding (bootstrap was rolled back to default 3.3.7 for example)

--- Tangy/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Tangy/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -10,6 +10,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using Tangy.Areas.Identity.Data;
+using Tangy.Data;
+using Tangy.Utility;
 
 namespace Tangy.Areas.Identity.Pages.Account
 {
@@ -20,17 +22,24 @@ namespace Tangy.Areas.Identity.Pages.Account
         private readonly UserManager<TangyUser> _userManager;
         private readonly ILogger<RegisterModel> _logger;
         private readonly IEmailSender _emailSender;
+        private readonly ApplicationDbContext _db;
+        private readonly RoleManager<IdentityRole> _roleManager;
 
         public RegisterModel(
             UserManager<TangyUser> userManager,
+            RoleManager<IdentityRole> roleManager,
             SignInManager<TangyUser> signInManager,
             ILogger<RegisterModel> logger,
-            IEmailSender emailSender)
+            IEmailSender emailSender,
+            ApplicationDbContext db
+            )
         {
             _userManager = userManager;
+            _roleManager = roleManager;
             _signInManager = signInManager;
             _logger = logger;
             _emailSender = emailSender;
+            _db = db;
         }
 
         [BindProperty]
@@ -79,10 +88,32 @@ namespace Tangy.Areas.Identity.Pages.Account
             returnUrl = returnUrl ?? Url.Content("~/");
             if (ModelState.IsValid)
             {
-                var user = new TangyUser { UserName = Input.Email, Email = Input.Email };
+                var user = new TangyUser
+                {
+                    UserName = Input.Email,
+                    Email = Input.Email,
+                    PhoneNumber = Input.PhoneNumber,
+                    FirstName = Input.FirstName,
+                    LastName = Input.LastName
+                };
+
                 var result = await _userManager.CreateAsync(user, Input.Password);
+
                 if (result.Succeeded)
                 {
+                    if (!await _roleManager.RoleExistsAsync(StaticDetails.AdminEndUser))
+                    {
+                        await _roleManager.CreateAsync(new IdentityRole(StaticDetails.AdminEndUser));
+                    }
+
+                    if (!await _roleManager.RoleExistsAsync(StaticDetails.CustomerEndUser))
+                    {
+                        await _roleManager.CreateAsync(new IdentityRole(StaticDetails.CustomerEndUser));
+                    }
+
+                    await _userManager.AddToRoleAsync(user, StaticDetails.AdminEndUser);
+
+
                     _logger.LogInformation("User created a new account with password.");
 
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
--- Tangy/Utility/StaticDetails.cs
+++ Tangy/Utility/StaticDetails.cs
@@ -8,5 +8,7 @@ namespace Tangy.Utility
     public class StaticDetails
     {
         public const string DefaultFoodImage = "\\images\\default-menu-item-img.jpg";
+        public const string AdminEndUser = "Admin";
+        public const string CustomerEndUser = "Customer";
     }
 }
