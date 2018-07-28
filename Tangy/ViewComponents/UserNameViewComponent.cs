e827d6d1368d467e5dc5a932ac2699fee19973b7
Tomasz Kaluzny <tkaluzny@outlook.com>
2018-07-28 15:49:34 +0100

    Added component

---
+++ Tangy/ViewComponents/UserNameViewComponent.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Tangy.Data;
+
+namespace Tangy.ViewComponents
+{
+    public class UserNameViewComponent : ViewComponent
+    {
+        private readonly ApplicationDbContext _db;
+
+        public UserNameViewComponent(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var userId = ((ClaimsPrincipal) this.User)?.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            
+            var user = await _db.Users.SingleOrDefaultAsync(u => u.Id == userId);
+
+            return View(user);
+        }
+    }
+}
